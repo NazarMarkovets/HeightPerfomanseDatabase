1b9d873f7ddaf9276e4c0655ed3bd2f327cc58c0
NazMarch <nazaran237@gmail.com>
2020-12-08 04:00:14 +0200

    Add indexes to MyIsam

--- scripts/Lab2/Update sequence/1) Update date from char TO Date format.sql
+++ scripts/Lab2/Update sequence/1) Update date from char TO Date format.sql
@@ -1,15 +1,15 @@
-update my.fonts SET fontsDATE = str_to_date(fontsDATE, "%m/%d/%Y");
-update my.macket_to_print SET macketDATE = str_to_date(macketDATE, "%m/%d/%Y");
+update my.fonts SET fonts_DATE = str_to_date(fonts_DATE, "%m/%d/%Y");
+update my.macket_to_print SET macket_DATE = str_to_date(macket_DATE, "%m/%d/%Y");
 update my.order SET order_start = str_to_date(order_start, "%m/%d/%Y");
 update my.order SET order_done = str_to_date(order_done, "%m/%d/%Y");
-update my.paper SET paperDATE = str_to_date(paperDATE, "%m/%d/%Y");
-update my.services SET serviceDATE = str_to_date(serviceDATE, "%m/%d/%Y");
+update my.paper SET paper_DATE = str_to_date(paper_DATE, "%m/%d/%Y");
+update my.services SET service_date = str_to_date(service_date, "%m/%d/%Y");
 update my.user SET registerDate = str_to_date(registerDate, "%m/%d/%Y");
 
-update inno.fonts SET fontsDATE = str_to_date(fontsDATE, "%m/%d/%Y");
-update inno.macket_to_print SET macketDATE = str_to_date(macketDATE, "%m/%d/%Y");
+update inno.fonts SET fonts_DATE = str_to_date(fonts_DATE, "%m/%d/%Y");
+update inno.macket_to_print SET macket_DATE = str_to_date(macket_DATE, "%m/%d/%Y");
 update inno.order SET order_start = str_to_date(order_start, "%m/%d/%Y");
 update inno.order SET order_done = str_to_date(order_done, "%m/%d/%Y");
-update inno.paper SET paperDATE = str_to_date(paperDATE, "%m/%d/%Y");
-update inno.services SET serviceDATE = str_to_date(serviceDATE, "%m/%d/%Y");
+update inno.paper SET paper_DATE = str_to_date(paper_DATE, "%m/%d/%Y");
+update inno.services SET service_date = str_to_date(service_date, "%m/%d/%Y");
 update inno.user SET registerDate = str_to_date(registerDate, "%m/%d/%Y");
