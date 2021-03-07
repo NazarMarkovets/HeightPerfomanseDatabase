6d050a0d9b2878518372777f8172931ef6877a7f
NazarMarkovets <nazaran237@gmail.com>
2021-03-07 13:39:31 +0200

    feat: added init files for project

---
+++ DataGenerator/DataExecuter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataExecuter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
